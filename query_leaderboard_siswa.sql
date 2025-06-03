11ce6d2536c7caac528ce126cbfe0ce2522b537b
FarrelXT <145682635+FarrelXT@users.noreply.github.com>
2025-06-03 19:21:47 +0700

    .

--- query_leaderboard_siswa.sql
+++ query_leaderboard_siswa.sql
@@ -1,3 +1,3 @@
-SELECT u.Username, r.poin FROM User u JOIN riwayat r ON u.Id_user = r.Id_user
+SELECT u.Username, r.point FROM User u JOIN riwayat r ON u.Id_user = r.Id_user
 WHERE u.jenis_user = 'siswa'
-ORDER BY r.poin DESC;
+ORDER BY r.point DESC;
