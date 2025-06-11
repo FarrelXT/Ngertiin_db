f48b7b38bbb76dfd3a434bb90505f2ed58920d40
FarrelXT <145682635+FarrelXT@users.noreply.github.com>
2025-06-12 00:46:24 +0700

    .

--- HOME/Select/repost_home.sql
+++ HOME/Select/repost_home.sql
@@ -1,7 +1,9 @@
 SELECT
+    u.username AS username_repost,
     r.isi_repost,
-    p.isi_post
-From repost r
+    p.isi_post    
+FROM repost r
 JOIN feedback f ON r.id_feedback = f.id_feedback
 JOIN post p ON f.id_post = p.id_post
+JOIN user u ON f.id_user = u.id_user
 WHERE f.id_post = {id_post}; -- placeholder post asli
